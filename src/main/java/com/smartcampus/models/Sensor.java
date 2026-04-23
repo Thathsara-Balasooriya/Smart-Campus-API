43d2b158b13ca29a1fb8cef8ce7cc2e35384cf12
Thathsara Gimhana <balasooriyatg@gmail.com>
2026-04-24 00:38:26 +0530

    feat(models): add Sensor model class
    Introduce a new entity class to represent sensor data within the system. The class includes fields for id, type, status, current value, and associated room, along with standard getters, setters, equals, and hashCode implementations.

---
+++ src/main/java/com/smartcampus/models/Sensor.java
@@ -0,0 +1,79 @@
+package com.smartcampus.models;
+
+import java.util.Objects;
+
+public final class Sensor {
+    private String id;
+    private String type;
+    private SensorStatus status;
+    private Double currentValue;
+    private String roomId;
+
+    public Sensor() {
+    }
+
+    public Sensor(String id, String type, SensorStatus status, Double currentValue, String roomId) {
+        this.id = id;
+        this.type = type;
+        this.status = status;
+        this.currentValue = currentValue;
+        this.roomId = roomId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public SensorStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(SensorStatus status) {
+        this.status = status;
+    }
+
+    public Double getCurrentValue() {
+        return currentValue;
+    }
+
+    public void setCurrentValue(Double currentValue) {
+        this.currentValue = currentValue;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Sensor)) {
+            return false;
+        }
+        Sensor sensor = (Sensor) o;
+        return Objects.equals(id, sensor.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
